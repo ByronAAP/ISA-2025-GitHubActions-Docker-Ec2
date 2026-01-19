1af16bfec6f8cf0455d5a26b8fd0d942abd0ee48
Byron Angulo <baangulop1@utn.edu.ec>
2026-01-19 10:24:41 -0500

    creacion de archivos

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hola():
+    return "<h1>¡Hola Byron!</h1>"
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
